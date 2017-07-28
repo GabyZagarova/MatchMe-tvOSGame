9c3e5450029ca337bb72a8be816552c7baaf1993
Gabriela <gabrielazagarova@Gabrielas-MacBook-Pro.local>
2017-07-28 15:37:21 +0300

    Initial Commit

---
+++ MatchMe/WelcomeViewController.swift
@@ -0,0 +1,46 @@
+//
+//  WelcomeViewController.swift
+//  MatchMe
+//
+//  Created on 10/19/15.
+//  Copyright © 2015. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+    @IBOutlet weak var gameTitle: UILabel!
+    @IBOutlet weak var gameDescription: UILabel!
+    @IBOutlet weak var enterNameLabel: UILabel!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var goButton: UIButton!
+    @IBOutlet weak var bestScoreButton: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.bestScoreButton.hidden = true
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    override var preferredFocusedView: UIView? {
+        return self.nameTextField
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        let defaults = NSUserDefaults.standardUserDefaults()
+        defaults.setValue(self.nameTextField.text, forKey: kCurrentUserNameKey)
+        defaults.synchronize()
+    }
+    
+    @IBAction func checkBestScoreButtonPressed(_: AnyObject) {
+        //TODO:
+        print("Best score tapped!")
+    }
+    
+}
+
---
+++ README.md
@@ -0,0 +1,23 @@
+# MatchMe-tvOSGame-Swift
+
+The game below will get your children to hone their memory skills and they won't even realise it! Most memory games encourage quiet and concentration too, and the more you play, the better you get...
+
+The purpose of this game is to be used during the tvOS Workshops and presentations. This Swift version is full. In the repo you can find folder with all need assets. Use Xcode, Parallax Preview Application or Photoshop plugin to create .lsr images.
+
+You can download both from here: https://developer.apple.com/tvos/download/
+
+####Restrictions:
+Xcode 7.1 and later
+Parallax Preview Application (Requires OS X Yosemite 10.10.5+ )
+Photoshop - Parallax Explorer (Requires OS X 10.9+, Photoshop CC 2014, 8 GB RAM)
+
+If you prefer to write on <b>Objective-C</b> - see the repo "MatchMe-tvOSGame-ObjectiveC":
+* https://github.com/MentorMate/MatchMe-tvOSGame-ObjectiveC
+
+
+Follow this link to find cropped versions:
+* https://github.com/siyana/MatchMe-tvOSGame-Cropped
+
+
+To check how iOS app is transitioned to tvOS app an to see the workshop agenda and more information about the speakers you can look at tvOSWorkshop project here:
+* https://github.com/MentorMate/tvOSWorkshop
